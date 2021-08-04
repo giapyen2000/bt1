03fff2acb4036c7d58c2491b220a4916aa936bdb
kikayen <giapthiyenvtvybg@gmail.com>
2021-08-04 16:55:38 +0700

    change bt2

---
+++ bt2.js
@@ -0,0 +1,160 @@
+/* 
+    Cho array listServers;
+    Dựa vào array "connects" để trả về list items serviceMap;
+    Vận dung kiến thưc javascript;
+
+    Example:
+
+    Input = [
+        {
+            "id": "1",
+            "title": "Service_01",
+            "connects": [
+                "2",
+                "8"
+            ]
+        }
+    ]
+
+    Output = [
+        {
+            "id": "1",
+            "title": "Service_01",
+            "connects": [
+                {
+                    "id": "2",
+                    "title": "Service_02",
+                    "connects": [
+                        "2",
+                        "7",
+                        "1"
+                    ]
+                },
+                {
+                    "id": "8",
+                    "title": "Service_08",
+                    "connects": [
+                        "4",
+                        "7"
+                    ]
+                },
+            ]
+        }
+    ]
+
+*/
+
+const listServers = [{
+        "id": "1",
+        "title": "Service_01",
+        "connects": [
+            "2",
+            "8"
+        ]
+    },
+    {
+        "id": "2",
+        "title": "Service_02",
+        "connects": [
+            "2",
+            "7",
+            "1"
+        ]
+    },
+    {
+        "id": "3",
+        "title": "Service_03",
+        "connects": [
+            "2",
+            "8"
+        ]
+    },
+    {
+        "id": "4",
+        "title": "Service_04",
+        "connects": [
+            "2",
+        ]
+    },
+    {
+        "id": "5",
+        "title": "Service_05",
+        "connects": [
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "id": "6",
+        "title": "Service_06",
+        "connects": [
+            "2",
+            "8",
+            "5"
+        ]
+    },
+    {
+        "id": "7",
+        "title": "Service_07",
+        "connects": [
+            "2",
+        ]
+    },
+    {
+        "id": "8",
+        "title": "Service_08",
+        "connects": [
+            "4",
+            "7"
+        ]
+    },
+]
+
+//cach cua anh khiem
+// let result = listServers.map(e => {
+//     let docs = e.connects.map(i => listServers.find(item => item.id == i))
+//     return {
+//         ...e,
+//         connects: docs
+//     }
+// })
+
+// console.log(JSON.stringify(result))
+var listServiceMap = [];
+
+
+function mapService(input) {
+
+    if (input.length > 0) {
+        input.forEach(e => {
+
+            var newServiceMap = {
+                id: e.id,
+                title: e.title,
+                connects: []
+            };
+            if (e.connects.length > 0) {
+                e.connects.forEach(id => {
+
+                    var server = listServers.find((i) => i.id === id);
+
+                    if (server !== undefined) {
+                        newServiceMap.connects.push(server);
+                    }
+                });
+            }
+
+            listServiceMap.push(newServiceMap);
+        });
+    }
+
+    return listServiceMap;
+};
+
+function result() {
+    mapService(listServers);
+    console.log(JSON.stringify(listServiceMap));
+};
+
+result();
